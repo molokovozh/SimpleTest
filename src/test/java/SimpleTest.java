14af8ebf5391426dd59b9012b46972220292ff3c
Admin <molokovozh@mail.ru>
2020-06-30 17:17:30 +0300

    Максимально упростил тест

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,6 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,20 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SimpleTest {
 
     protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(SimpleTest.class);
 
     @Before
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        logger.info("Драйвер поднят");
     }
     @Test
     public void openPage() {
         driver.manage().window().maximize();
         driver.get("https://otus.ru/");
         System.out.println(driver.getTitle());
-        logger.info("Ура, открыта страница OTUS!");
     }
 
     @After
